22df2914a42d662e0a80d8639d96b76b281eb012
Ilya Shcherbakov <shcherbakov.illya@list.ru>
2023-12-23 19:47:11 +0300

    first edit

---
+++ src/components/users.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import api from "../api";
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const handleDelete = (userId) => {
+    setUsers(users.filter((user) => user._id !== userId));
+  };
+  const renderPhrase = (number) => {
+    if (number > 4 && number < 15) return "челов готовы";
+    if (number === 2 || number === 3 || number === 4) return "чела готовы";
+    if (number === 1) return "чел готов";
+  };
+  return (
+    <>
+      <h2>
+        <span
+          className={"badge bg-" + (users.length > 0 ? "primary" : "danger")}
+        >
+          {users.length > 0
+            ? users.length + ` ${renderPhrase(users.length)} с тобой тусануть`
+            : "Никто с тобой не тусанёт"}
+        </span>
+      </h2>
+      {users.length > 0 && (
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((item) => (
+                    <span
+                      className={"badge m-1 bg-" + item.color}
+                      key={item._id}
+                    >
+                      {item.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate}</td>
+                <td>
+                  <button
+                    className={"btn btn-danger"}
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    Удалить
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
+export default Users;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 //import App from './App';
 //import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import Users from "./components/users";
 
-const element = <h1>I am FrontEnd developer</h1>
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {element}
+    <Users />
   </React.StrictMode>
 );
-
-//auto import
-//material icon theme
-//prettier
